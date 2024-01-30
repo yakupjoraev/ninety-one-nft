d13d5308ff1f430639d00e86668d6872dbb30f10
bekndev <bekndev@gmail.com>
2024-01-30 14:18:28 +0500

    fixed

--- 91coin-backend/src/app.service.ts
+++ 91coin-backend/src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello 91ninetyone!';
+  getHello(): any {
+    throw new HttpException(
+      'Hello 91ninetyone!',
+      HttpStatus.OK
+    )
   }
 }
